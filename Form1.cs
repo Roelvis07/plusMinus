bc750efa704c258d935c7595c6bebce6282107b0
Roelvis Coello <rcabijana@gmail.com>
2022-06-13 01:31:29 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plusMinus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<int> arr = new List<int>() { 1, 2, 3, -1, -2, -3, 0, 0};
+            plusMinus(arr);
+        }
+        public static void plusMinus(List<int> arr)
+        {
+            double plus = 0, minus = 0, zeros = 0;
+
+            for(int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] == 0) zeros++;
+                if (arr[i] < 0) minus++;
+                if (arr[i] > 0) plus++;
+            }
+
+            Console.WriteLine(Math.Round(plus / arr.Count, 6));
+            Console.WriteLine(Math.Round(minus / arr.Count, 6));
+            Console.WriteLine(Math.Round(zeros / arr.Count, 6));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("plusMinus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("plusMinus")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f4a02b3-1c76-4275-a75c-dc6e504246d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
